c7108d145eebff4b95cab7f81e46714f0602b65f
Oziel Pacheco <latikakm@gmail.com>
2022-02-08 18:39:32 -0600

    Se carga bootstrap-icons y se avanza con el formulario

--- src/app/components/compras/compra/compra-agregar/compra-agregar.component.ts
+++ src/app/components/compras/compra/compra-agregar/compra-agregar.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+/**Servicios */
+import { ProveedorService } from 'src/app/services/proveedor.service';
 
 @Component({
   selector: 'app-compra-agregar',
   templateUrl: './compra-agregar.component.html',
-  styleUrls: ['./compra-agregar.component.css']
+  styleUrls: ['./compra-agregar.component.css'],
+  providers: [ProveedorService]
 })
 export class CompraAgregarComponent implements OnInit {
 
-  constructor() { }
+  public proveedores: any;
+
+  constructor( private _proveedorService: ProveedorService) { }
 
   ngOnInit(): void {
+    this.getProvee();
   }
-
+/**SERVICIOS */
+  getProvee(){
+    this._proveedorService.getProveedores().subscribe(
+      response => {
+        if(response.status == 'success'){
+          this.proveedores = response.proveedores;
+          console.log(this.proveedores);
+        }
+      },
+      error =>{
+        console.log(error);
+      }
+    );
+  }
 }
