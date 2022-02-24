86e26f69eb5ae3173e227424bbc4e53468e53d34
Oziel Pacheco <latikakm@gmail.com>
2022-02-24 14:55:52 -0600

    logica al cargar productos a lista de compras

--- src/app/components/compras/ordenes-compras/ordencompra-agregar/ordencompra-agregar.component.ts
+++ src/app/components/compras/ordenes-compras/ordencompra-agregar/ordencompra-agregar.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { ProveedorService } from 'src/app/services/proveedor.service';
 import { ProductoService } from 'src/app/services/producto.service';
 import { global } from 'src/app/services/global';
+import { ToastService } from 'src/app/services/toast.service';
 //Modelos
 import { Ordencompra } from 'src/app/models/orden_compra';
 import { Producto_orden } from 'src/app/models/producto_orden';
@@ -25,6 +26,7 @@ export class OrdencompraAgregarComponent implements OnInit {
   public producto_orden: Producto_orden;
   public Lista_compras: Array<Producto_orden>;
   public productos: any;
+  public isSearch: boolean = true;
   /**PAGINATOR */
   public totalPages: any;
   public page: any;
@@ -44,7 +46,8 @@ export class OrdencompraAgregarComponent implements OnInit {
 
   constructor( private _proveedorService: ProveedorService,
       private modalService: NgbModal,
-      private _productoService: ProductoService
+      private _productoService: ProductoService,
+      public toastService: ToastService
     ) {
     this.orden_compra = new Ordencompra(0,null,0,'',null,0,0,null);
     this.producto_orden = new Producto_orden(0,0,0,null,null,null,null);
@@ -59,21 +62,34 @@ export class OrdencompraAgregarComponent implements OnInit {
   onChange(id:any){//evento que muestra los datos del proveedor al seleccionarlo
     this.getProveeVer(id);
   }
-  cambioSeleccionado(e:any){//limpiamos los inputs
+  cambioSeleccionado(e:any){//limpiamos los inputs del modal
     this.buscarProducto = '';
     this.buscarProductoCE = '';
     this.buscarProductoCbar = '';
   }
-  capturar(datos:any){
-    this.Lista_compras.push({...this.producto_orden});
+  capturar(datos:any){//Agregar a lista de compras
+    if(this.producto_orden.cantidad == 0){
+      this.toastService.show('No se pueden agregar productos con cantidad: 0',{classname: 'bg-danger text-light', delay: 6000})
+    }else if(this.producto_orden.idProducto == 0){
+      this.toastService.show('Ese producto no existe',{classname: 'bg-danger text-light', delay: 6000})
+    }else if( this.Lista_compras.find( x => x.idProducto == this.producto_orden.idProducto)){
+      //verificamos si la lista de compras ya contiene el producto buscandolo por idProducto
+      this.toastService.show('Ese producto ya esta en la lista',{classname: 'bg-danger text-light', delay: 6000})
+    }else{
+      this.Lista_compras.push({...this.producto_orden});
+      this.isSearch=true;
+    }
+    
     //console.log(this.Lista_compras);
-  }
-  consultarProducto(event:any){
+  } 
+  consultarProducto(event:any){//mostrar informacion del producto al dar enter
     if (event.keyCode === 13) {
       //alert('you just pressed the enter key'+event);
       this.dato=event.target.value;
       //console.log(this.dato)
       this.getProd(this.dato);
+      this.isSearch = false;
+      //console.log(this.producto_orden);
     }
   }
   //Servicios
@@ -95,7 +111,7 @@ export class OrdencompraAgregarComponent implements OnInit {
       response => {
         if(response.status == 'success'){
           this.proveedorVer = response.proveedores;
-          console.log(this.proveedorVer);
+          //console.log(this.proveedorVer);
         }
       }, error =>{
           console.log(error);
@@ -120,7 +136,10 @@ export class OrdencompraAgregarComponent implements OnInit {
   getProd(id:any){
     this._productoService.getProdclaveex(id).subscribe(
       response =>{
-        this.productoVer = response.producto;
+        this.productoVer = response.producto;//informacion completa del producto para recorrerlo atraves del html
+        this.producto_orden.descripcion = this.productoVer[0]['descripcion'];//asignamos variables
+        this.producto_orden.idProducto = this.productoVer[0]['idProducto'];
+        this.producto_orden.nombreMedida = this.productoVer[0]['nombreMedida'];
         //console.log(this.productoVer);
       },error => {
         console.log(error);
@@ -135,7 +154,6 @@ export class OrdencompraAgregarComponent implements OnInit {
       this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
     });
   }
-
   private getDismissReason(reason: any): string {
     if (reason === ModalDismissReasons.ESC) {
       return 'by pressing ESC';
