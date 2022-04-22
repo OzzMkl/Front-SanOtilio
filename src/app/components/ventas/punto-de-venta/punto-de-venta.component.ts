3723ad690a00e4e704186af493c410e352db9001
Oziel Pacheco <latikakm@gmail.com>
2022-04-21 20:24:21 -0500

    creacion modelo ventag y avance diseño ventas

--- src/app/components/ventas/punto-de-venta/punto-de-venta.component.ts
+++ src/app/components/ventas/punto-de-venta/punto-de-venta.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+//servicio
+import { ClientesService } from 'src/app/services/clientes.service';
+//modelos
+import { Ventag } from 'src/app/models/ventag';
+//NGBOOTSTRAP-modal
+import { NgbModal, ModalDismissReasons, NgbDateStruct} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-punto-de-venta',
@@ -6,10 +12,77 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./punto-de-venta.component.css']
 })
 export class PuntoDeVentaComponent implements OnInit {
+  //cerrar modal
+  closeResult = '';
+  //variable de servicios
+  public clientes:any;//getClientes
+  public cliente:any;//seleccionarCliente
+  public dirCliente:any;//seleccionarCliente
+   /**PAGINATOR */
+   public totalPages: any;
+   public page: any;
+   public next_page: any;
+   public prev_page: any;
+   pageActual: number = 1;
+   //pipe de busqueda en modal
+   buscarCliente ='';
+   //modelo de venta
+   public ventag: Ventag;
+   //
+   public seEnvia: boolean = false;
 
-  constructor() { }
+
+  constructor( private modalService: NgbModal, private _clienteService: ClientesService) {
+    this.ventag = new Ventag(0,0,0,0,'',0,null,0,'');
+   }
 
   ngOnInit(): void {
   }
-
+  //traemos todos los clientes
+  getClientes(){
+    this._clienteService.getAllClientes().subscribe( 
+      response =>{
+        if(response.status == 'success'){
+          this.clientes = response.clientes;
+          //console.log(this.clientes);
+        }
+      },error =>{
+      console.log(error);
+    });
+  }
+  //traemos la informacion del cliente seleccionado
+  seleccionarCliente(idCliente:any){
+    this._clienteService.getDetallesCliente(idCliente).subscribe( 
+      response =>{
+        if(response.status == 'success'){
+          this.cliente = response.cliente;
+          this.dirCliente= response.cdireccion;
+          this.ventag.nombreCliente = this.cliente[0]['nombre']+' '+this.cliente[0]['aPaterno']+' '+this.cliente[0]['aMaterno'];
+        }else{
+          console.log('algo salio mal'+response);
+        }
+        console.log(response.cliente);
+        //console.log(response.cdireccion);
+      },error=>{
+        console.log(error);
+      });
+  }
+  // Modal
+  open(content:any) {
+    this.getClientes();
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title', size: 'xl'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
 }
---
+++ src/app/models/ventag.ts
@@ -0,0 +1,14 @@
+export class Ventag{//Modelo de las medidas aqui asignamos todas las variables de acuerdo a la tabla que se tiene en la base de datos
+    constructor(
+        public idVenta: number,
+        public idCliente: number,
+        public idTipoVenta: number,
+        public idTipoPago: number,
+        public observaciones: string,
+        public idStatus: number,
+        public fecha: any,
+        public idEmpleado: number,
+        //extras
+        public nombreCliente:string
+    ){}
+}
