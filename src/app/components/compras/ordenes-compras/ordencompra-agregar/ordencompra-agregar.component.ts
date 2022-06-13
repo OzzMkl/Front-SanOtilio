38908c118c751a8ef32c2973c9ec56648534194d
Oziel Pacheco <latikakm@gmail.com>
2022-06-13 12:03:54 -0500

    Arreglo de errores

--- src/app/components/compras/ordenes-compras/ordencompra-agregar/ordencompra-agregar.component.ts
+++ src/app/components/compras/ordenes-compras/ordencompra-agregar/ordencompra-agregar.component.ts
@@ -68,7 +68,7 @@ export class OrdencompraAgregarComponent implements OnInit {
       public _empleadoService : EmpleadoService
     ) {
     this.orden_compra = new Ordencompra(0,null,0,'',null,0,1,null);
-    this.producto_orden = new Producto_orden(0,0,0,null,null,null,null);
+    this.producto_orden = new Producto_orden(0,0,'','',0,'');
     this.Lista_compras = [];
     this.url = global.url;
     
@@ -77,7 +77,7 @@ export class OrdencompraAgregarComponent implements OnInit {
   ngOnInit(): void {
     this.getProvee();
     this.getAllProducts();
-    this.loadUser();
+    this.loadUser(); 
     //this.createPDF();
     //this.getDetailsOrd();
   }
