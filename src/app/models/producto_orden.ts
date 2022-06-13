6c30dbd680b9251cc8a30daf3f568f8c44d3c8e2
Oziel Pacheco <latikakm@gmail.com>
2022-06-13 12:12:05 -0500

    cambios producto orden

--- src/app/models/producto_orden.ts
+++ src/app/models/producto_orden.ts
@@ -2,11 +2,11 @@ export class Producto_orden{//Modelo de la compra
     constructor(
         public idOrd: number,
         public idProducto: number,
-        public claveEx: any,
-        public nombreMedida: any,
+        public claveEx: string,
+        public nombreMedida: string,
         public cantidad: number,
         /**Se agregan campos extras para mostrar en la vista de la lista de compras */
-        public descripcion: any
+        public descripcion: string
 
     ){}
 }
