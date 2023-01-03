7302d97b0037a0bbafe5be5356992ab9950f7e27
Oziel Pacheco <latikakm@gmail.com>
2023-01-02 18:57:29 -0600

    actualizado a v14

--- src/app/services/cajas.service.ts
+++ src/app/services/cajas.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders} from '@angular/common/http';
 import { Observable } from "rxjs";
-import { global } from "./global"; 
-import { parseTranslation } from '@angular/localize/src/utils';
+import { global } from "./global";
 
 @Injectable({
   providedIn: 'root'
