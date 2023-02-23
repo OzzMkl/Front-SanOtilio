639b1443164807cce0ecf06f6b421376ae1c3b07
Paco <pacosalomon6043@gmail.com>
2023-02-22 20:49:54 -0600

    Avances compra-buscar

--- src/app/components/compras/compra/compra-buscar/compra-buscar.component.ts
+++ src/app/components/compras/compra/compra-buscar/compra-buscar.component.ts
@@ -1,15 +1,108 @@
 import { Component, OnInit } from '@angular/core';
+import { CompraService } from 'src/app/services/compra.service';
+import { HttpClient} from '@angular/common/http';
+import { Router} from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-compra-buscar',
   templateUrl: './compra-buscar.component.html',
-  styleUrls: ['./compra-buscar.component.css']
+  styleUrls: ['./compra-buscar.component.css'],
+  providers: [CompraService]
 })
 export class CompraBuscarComponent implements OnInit {
 
-  constructor() { }
+  //public proveedores: Array<Proveedor>;
+  public compras: Array<any> = [];
+  public totalPages: any;
+  public path: any;
+  public next_page: any;
+  public prev_page: any;
+  public itemsPerPage:number=0;
+  pageActual: number = 0;
+
+  public tipoBusqueda: number = 1;
+  //spinner
+  public isLoading:boolean = false;
+  //Subscripciones
+  private getCompraSub : Subscription = new Subscription;
+
+  constructor(
+    private _compraService: CompraService,
+    private _router: Router,
+    private _http: HttpClient
+  ) {}
 
   ngOnInit(): void {
+    this.getComprasR();
+  }
+
+  //
+  
+  getComprasR(){
+    //mostramos el spinner
+    this.isLoading = true;
+
+
+    this.getCompraSub = this._compraService.getComprasRecibidas().subscribe(
+      response =>{
+        if(response.status == 'success'){
+
+          this.compras = response.compra.data;
+          //navegacion de paginacion
+          this.totalPages = response.compra.total;
+          this.itemsPerPage = response.compra.per_page;
+          this.pageActual = response.compra.current_page;
+          this.next_page = response.compra.next_page_url;
+          this.path = response.compra.path;
+
+          //una vez terminado quitamos el spinner
+          this.isLoading=false;
+        }
+      },
+      error =>{
+        console.log(error);
+      }
+    );
+  }
+
+  /**
+   * 
+   * @param page
+   * Es el numero de pagina a la cual se va acceder
+   * @description
+   * De acuerdo al numero de pagina recibido lo concatenamos a
+   * la direccion para "ir" a esa direccion y traer la informacion
+   * no retornamos ya que solo actualizamos las variables a mostrar
+   */
+  getPage(page:number) {
+    //iniciamos spinner
+    this.isLoading = true;
+
+    this._http.get(this.path+'?page='+page).subscribe(
+      (response:any) => {
+        
+        this.compras = response.compra.data;
+        //navegacion de paginacion
+        this.totalPages = response.compra.total;
+        this.itemsPerPage = response.compra.per_page;
+        this.pageActual = response.compra.current_page;
+        this.next_page = response.compra.next_page_url;
+        this.path = response.compra.path;
+
+        //una vez terminado quitamos el spinner
+        this.isLoading=false;        
+    })
+  }
+
+  
+
+   /**
+   * Destruye las subscripciones a los observables de regitro proveedor
+   * y obtecion de bancos
+   */
+   ngOnDestroy(): void {
+    this.getCompraSub.unsubscribe();
   }
 
 }
--- src/app/services/compra.service.ts
+++ src/app/services/compra.service.ts
@@ -9,43 +9,46 @@ import { global } from "./global";
 export class CompraService {
 
   public url : string;
+  public headers:any = new HttpHeaders().set('Content-Type','application/x-www-form-urlencoded');
 
   constructor( public _http: HttpClient ) { this.url = global.url; }
 
   registrerCompra(compra:any):Observable<any>{
     let json = JSON.stringify(compra);
     let params = 'json='+json;
-    let headers = new HttpHeaders().set('Content-Type','application/x-www-form-urlencoded');//mandamos el json con las cabeceras para que obtengamos el token
-    return this._http.post(this.url+'compra/register',params, {headers:headers} );
+    return this._http.post(this.url+'compra/register',params, {headers:this.headers} );
   }
 
   getLastCompra():Observable<any>{
-    let headers = new HttpHeaders().set('Content-Type','application/x-www-form-urlencoded');
-    return this._http.get(this.url+'compra/getLastCompra', {headers:headers} );
+    return this._http.get(this.url+'compra/getLastCompra', {headers:this.headers} );
   }
 
   getDetailsCompra(idCompra:any):Observable<any>{
-    let headers = new HttpHeaders().set('Content-Type','application/x-www-form-urlencoded');
-    return this._http.get(this.url+'compra/showMejorado/'+idCompra, {headers:headers} );
+    return this._http.get(this.url+'compra/showMejorado/'+idCompra, {headers:this.headers} );
   }
 
   registerProductoscompra(ProductosCompra:any):Observable<any>{
     let json = JSON.stringify(ProductosCompra);
     let params = 'json='+json;
-    let headers = new HttpHeaders().set('Content-Type','application/x-www-form-urlencoded');//mandamos el json con las cabeceras para que obtengamos el token
-    return this._http.post(this.url+'compra/registerLista',params, {headers:headers} );
+    return this._http.post(this.url+'compra/registerLista',params, {headers:this.headers} );
   }
 
   updateExistencia(ProductosCompra:any):Observable<any>{
     let json = JSON.stringify(ProductosCompra);
     let params = 'json='+json;
-    let headers = new HttpHeaders().set('Content-Type','application/x-www-form-urlencoded');//mandamos el json con las cabeceras para que obtengamos el token
-    return this._http.post(this.url+'compra/updateExistencia',params, {headers:headers} );
+    return this._http.post(this.url+'compra/updateExistencia',params, {headers:this.headers} );
   }
 
   registerLote():Observable<any>{
-    let headers = new HttpHeaders().set('Content-Type','application/x-www-form-urlencoded');//mandamos el json con las cabeceras para que obtengamos el token
-    return this._http.post(this.url+'compra/registerLote', {headers:headers} );
+    return this._http.post(this.url+'compra/registerLote', {headers:this.headers} );
+  }
+
+  getComprasRecibidas():Observable<any>{
+    return this._http.get(this.url+'compra/listaComprasRecibidas', {headers:this.headers} );
+  }
+
+  getsearchIdCompra(idCompra:any):Observable<any>{
+    return this._http.get(this.url+'compra/searchIdCompra/'+idCompra, {headers:this.headers} );
   }
 
 }
