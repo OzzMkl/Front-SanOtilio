a62575b73644cdf13b2576bc33703163778c31e2
Paco <pacosalomon6043@gmail.com>
2023-07-12 08:53:03 -0600

    Se retiran imports

--- src/app/components/compras/ordenes-compras/ordencompra-editar/ordencompra-editar.component.ts
+++ src/app/components/compras/ordenes-compras/ordencompra-editar/ordencompra-editar.component.ts
@@ -13,9 +13,6 @@ import { Ordencompra } from 'src/app/models/orden_compra';
 import { Producto_orden } from 'src/app/models/producto_orden';
 //Modal
 import { NgbDateStruct, NgbModal,ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
-//pdf
-import jsPDF from 'jspdf';
-import autoTable from 'jspdf-autotable';
 
 @Component({
   selector: 'app-ordencompra-editar',
@@ -196,7 +193,7 @@ pageActual2: number = 0;
       this._ordencompraService.getDetailsOrdes(id).subscribe(
         response =>{
           if(response.status  == 'success' && response.ordencompra.length > 0 && response.productos.length > 0){
-
+ 
             //asignamos de uno en uno las propiedades de la orden
             this.orden_compra.idProveedor = response.ordencompra[0]['idProveedor'];
             this.orden_compra.fecha = response.ordencompra[0]['fecha'];
@@ -299,46 +296,7 @@ pageActual2: number = 0;
     this.identity = this._empleadoService.getIdentity();
   }
   createPDF():void{//creamos el pdf
-    const doc = new jsPDF
-    var logo = new Image();//CREAMOS VARIABLE
-    logo.src = 'assets/images/logo-solo.png';//ASIGNAMOS LA UBICACION DE LA IMAGEN
-    var nombreE = this.detailOrd[0]['nombreEmpleado'];//concatenamos el nombre completo
-    var cabeceras = ["CLAVE EXTERNA","DESCRIPCION","MEDIDA","CANTIDAD"];
-    var rows:any = [];
-    doc.setDrawColor(255, 145, 0);//AGREGAMOS COLOR NARANJA A LAS LINEAS
-
-    //           ancho linea   x1,y1  x2,y2
-    doc.setLineWidth(2.5).line(10,10,200,10);//colocacion de linea
-    doc.setLineWidth(2.5).line(50,15,160,15);
-    //          tipografia       tamaño letra       texto                         x1,y1
-    doc.setFont('Helvetica').setFontSize(16).text('MATERIALES PARA CONSTRUCCION', 55,25);
-    doc.setFont('Helvetica').setFontSize(16).text(" \"SAN OTILIO\" ", 85,30);
-    // variable con logo, tipo x1,y1, ancho, largo
-    doc.addImage(logo,'PNG',100,32,10,10);
-    doc.setFont('Helvetica').setFontSize(10).text('REPORTE DE ORDEN DE COMPRA', 10,50);
-    doc.setLineWidth(2.5).line(10,53,70,53);
-    //           tipografia,negrita        tamaño          texto              x1,y1
-    doc.setFont('Helvetica','bold').setFontSize(10).text('NO. ORDEN: '+this.detailOrd[0]['idOrd'], 10,60);
-    doc.setFont('Helvetica','normal').setFontSize(10).text('FECHA IMPRESION: '+this.fecha.toLocaleDateString(), 50,65);
-    doc.setFont('Helvetica','normal').setFontSize(10).text('FECHA ESTIMADA: '+this.detailOrd[0]['fecha'], 115,65);  
-
-    doc.setLineWidth(1).line(10,70,200,70);
-    doc.setFont('Helvetica','normal').setFontSize(10).text('REALIZO: '+ nombreE.toUpperCase(), 10,75);
-    doc.setFont('Helvetica','normal').setFontSize(10).text('PROVEEDOR: '+this.detailOrd[0]['nombreProveedor'], 10,80);
-    doc.setFont('Helvetica','normal').setFontSize(10).text('OBSERVACIONES: '+this.detailOrd[0]['observaciones'], 10,85);
-
-
-    doc.setLineWidth(1).line(10,92,200,92);
-    //recorremos los productos con un foreah
-    this.productosdetailOrd.forEach((element:any) => {
-      var temp = [ element.claveEx,element.descripcion,element.nombreMedida,element.cantidad];//creamos variable "temporal" y asignamos 
-      rows.push(temp);//añadimos a fila
-    });
-    //generamos la tabla
-    autoTable(doc,{ head:[cabeceras],
-    body:rows, startY:95 })
-    //creamos el pdf
-    doc.save("OrdenModificada-"+this.detailOrd[0]['idOrd']+".pdf");
+    
     //doc.save('a.pdf')
   }
   getOrderModificada(){//reconsultamos la informacion de la orden modificada
