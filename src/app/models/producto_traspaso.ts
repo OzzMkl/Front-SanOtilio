03a1dc96c1f149888a6e2466cfa6375243eb4812
Paco <pacosalomon6043@gmail.com>
2023-09-01 17:27:36 -0600

    Creacion de modelos para traspaso

---
+++ src/app/models/producto_traspaso.ts
@@ -0,0 +1,17 @@
+export class Producto_traspaso{//Modelo de la compra
+    constructor(
+        public idTraspaso: number,
+        public idProducto: number,
+        public descripcion: any,
+        public claveexterna: any,
+        public idProdMedida:number,
+        public cantidad: number,
+        public precio: number,
+        public subtotal: any,
+        public igualMedidaMenor: any,
+        public updated_at: any,
+        /**Se agregan campos extras para mostrar 
+         * en la vista de la lista de compras */
+        public nombreMedida: any
+    ){}
+}
