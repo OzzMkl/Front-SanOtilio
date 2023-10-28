4f676ae928c4299b1b5fe066d751910436cb3af9
Oziel Pacheco <ozielpacheco.m@gmail.com>
2023-10-27 21:09:49 -0600

    Se agrega url de DEV

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,6 +1,8 @@
 export var global = {
     //LOCAL
     url: 'http://192.168.200.6/backend-Laravel/api-laravel/public/api/'
+    // DEV
+    // url: 'http://192.168.7.3/backend-Laravel/api-laravel/public/api/'
     //CHILAC
     //url: 'http://192.168.7.45/backend-Laravel/api-laravel/public/api/'
     //VIVEROS
