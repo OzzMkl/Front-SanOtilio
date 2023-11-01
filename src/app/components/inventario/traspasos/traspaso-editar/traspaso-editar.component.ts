fdabe98a4e843790d2b003297714c6d41991c959
Paco <pacosalomon6043@gmail.com>
2023-10-31 20:30:16 -0600

    Se agrega metodo para obtener informacion

--- src/app/components/inventario/traspasos/traspaso-editar/traspaso-editar.component.ts
+++ src/app/components/inventario/traspasos/traspaso-editar/traspaso-editar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+//Servicios
+import { TraspasoService } from 'src/app/services/traspaso.service';
 
 @Component({
   selector: 'app-traspaso-editar',
@@ -6,10 +9,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./traspaso-editar.component.css']
 })
 export class TraspasoEditarComponent implements OnInit {
+  //Datos de traspaso
+  public detallesTraspaso:any;
+  public productosDT:any;
 
-  constructor() { }
+  constructor(
+    private _route: ActivatedRoute,
+    private _traspasoService: TraspasoService
+
+  ) { }
 
   ngOnInit(): void {
+    this.getDatosRuta();
   }
+
+  getDatosRuta(){
+    this._route.params.subscribe( params =>{
+      let idTraspaso:number = params['idTraspaso'];//la asignamos en una variable
+      let tipoTraspaso:string = params['tipoTraspaso'];//la asignamos en una variable
+      // console.log(params);
+      // console.log(idTraspaso);
+      // console.log(tipoTraspaso);
+      this.getDetailsTraspaso(idTraspaso,tipoTraspaso);
+    });
+  }
+
+  getDetailsTraspaso(idTraspaso:any,tipoTraspaso:any){
+    //console.log(idTraspaso);
+    //console.log(tipoTraspaso);
+    this._traspasoService.getDetailsTraspaso(idTraspaso,tipoTraspaso).subscribe(
+      response =>{
+        if(response.status == 'success'){
+          this.detallesTraspaso = response.traspaso; 
+          this.productosDT = response.productos;
+          //console.log('response',response);
+          console.log('traspaso',this.detallesTraspaso);
+          console.log('productos',this.productosDT);
+
+        }else{ console.log('Algo salio mal'); }
+        
+      },error => {
+        console.log(error);
+      });
+  }
+
+  
 
 }
