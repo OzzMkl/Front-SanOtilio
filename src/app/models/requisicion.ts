ff5b354c2fb1cd2ec4df700a7ba0d55edcb7b557
Francisco <pacosalomon6043@gmail.com>
2024-05-07 19:15:08 -0600

    Modificacion de model, se agrega idProveedor

--- src/app/models/requisicion.ts
+++ src/app/models/requisicion.ts
@@ -1,6 +1,7 @@
 export class Requisicion{//Modelo de la compra
     constructor(
         public idReq: number,
+        public idProveedor: number,
         public observaciones: string,
         public idEmpleado: number,
         public idStatus: number,
