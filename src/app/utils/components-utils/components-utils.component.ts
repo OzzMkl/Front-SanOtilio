9201f4d614e200e172ce62476f04665a9d9a3429
Oziel Pacheco <ozielpacheco.m@gmail.com>
2024-05-15 12:58:25 -0600

    Creacion de componentes y servicios - Alta de componente modal-ventas - Alta de servicio mdlVentasService - Se agrega al components-utils para su exportacion

--- src/app/utils/components-utils/components-utils.component.ts
+++ src/app/utils/components-utils/components-utils.component.ts
@@ -4,17 +4,20 @@ import { PrimengModule } from 'src/app/primeng/primeng/primeng.module';
 
 import { LoadingSpinnerComponent } from './loading-spinner/loading-spinner.component';
 import { ModalProductosComponent } from './modal-productos/modal-productos.component';
+import { ModalVentasComponent } from './modal-ventas/modal-ventas.component';
 import { InputExternalKeySearchComponent } from './input-external-key-search/input-external-key-search.component';
 
 @NgModule({
     declarations: [
         LoadingSpinnerComponent,
         ModalProductosComponent,
+        ModalVentasComponent,
         InputExternalKeySearchComponent,
     ],
     exports: [
         LoadingSpinnerComponent,
         ModalProductosComponent,
+        ModalVentasComponent,
         InputExternalKeySearchComponent,
     ],
     imports : [
---
+++ src/app/utils/components-utils/modal-ventas/modal-ventas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-ventas',
+  templateUrl: './modal-ventas.component.html',
+  styleUrls: ['./modal-ventas.component.css']
+})
+export class ModalVentasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
